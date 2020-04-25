41fe923d6bb2a313d953625452d883045a3d5b54
lbernier <leandro_bernier@yahoo>
2020-04-25 16:37:47 -0400

    supresión de los métodos y código no  utilizados

--- src/app/edit-app/edit-app.component.ts
+++ src/app/edit-app/edit-app.component.ts
@@ -7,158 +7,157 @@ import swal from 'sweetalert2';
 @Component({
   selector: 'app-edit-app',
   templateUrl: './edit-app.component.html',
-  styleUrls: ['./edit-app.component.css']
+  styleUrls: ['./edit-app.component.css'],
 })
 export class EditAppComponent implements OnInit {
-
   value: value = {
-    label: "",
-    value: ""
+    label: '',
+    value: '',
   };
   success = false;
 
   fieldModels: Array<field> = [
     {
-      "type": "text",
-      "icon": "fa-font",
-      "label": "Text",
-      "description": "Enter your name",
-      "placeholder": "Enter your name",
-      "className": "form-control",
-      "subtype": "text",
-      "regex": "",
-      "handle": true
+      type: 'text',
+      icon: 'fa-font',
+      label: 'Text',
+      description: 'Enter your name',
+      placeholder: 'Enter your name',
+      className: 'form-control',
+      subtype: 'text',
+      regex: '',
+      handle: true,
     },
     {
-      "type": "email",
-      "icon": "fa-envelope",
-      "required": true,
-      "label": "Email",
-      "description": "Enter your email",
-      "placeholder": "Enter your email",
-      "className": "form-control",
-      "subtype": "text",
-      "regex": "^([a-zA-Z0-9_.-]+)@([a-zA-Z0-9_.-]+)\.([a-zA-Z]{2,5})$",
-      "errorText": "Please enter a valid email",
-      "handle": true
+      type: 'email',
+      icon: 'fa-envelope',
+      required: true,
+      label: 'Email',
+      description: 'Enter your email',
+      placeholder: 'Enter your email',
+      className: 'form-control',
+      subtype: 'text',
+      regex: '^([a-zA-Z0-9_.-]+)@([a-zA-Z0-9_.-]+).([a-zA-Z]{2,5})$',
+      errorText: 'Please enter a valid email',
+      handle: true,
     },
     {
-      "type": "phone",
-      "icon": "fa-phone",
-      "label": "Phone",
-      "description": "Enter your phone",
-      "placeholder": "Enter your phone",
-      "className": "form-control",
-      "subtype": "text",
-      "regex": "^[\+]?[(]?[0-9]{3}[)]?[-\s\.]?[0-9]{3}[-\s\.]?[0-9]{4,6}$",
-      "errorText": "Please enter a valid phone number",
-      "handle": true
+      type: 'phone',
+      icon: 'fa-phone',
+      label: 'Phone',
+      description: 'Enter your phone',
+      placeholder: 'Enter your phone',
+      className: 'form-control',
+      subtype: 'text',
+      regex: '^[+]?[(]?[0-9]{3}[)]?[-s.]?[0-9]{3}[-s.]?[0-9]{4,6}$',
+      errorText: 'Please enter a valid phone number',
+      handle: true,
     },
     {
-      "type": "number",
-      "label": "Number",
-      "icon": "fa-html5",
-      "description": "Age",
-      "placeholder": "Enter your age",
-      "className": "form-control",
-      "value": "20",
-      "min": 12,
-      "max": 90
+      type: 'number',
+      label: 'Number',
+      icon: 'fa-html5',
+      description: 'Age',
+      placeholder: 'Enter your age',
+      className: 'form-control',
+      value: '20',
+      min: 12,
+      max: 90,
     },
     {
-      "type": "date",
-      "icon": "fa-calendar",
-      "label": "Date",
-      "placeholder": "Date",
-      "className": "form-control"
+      type: 'date',
+      icon: 'fa-calendar',
+      label: 'Date',
+      placeholder: 'Date',
+      className: 'form-control',
     },
     {
-      "type": "datetime-local",
-      "icon": "fa-calendar",
-      "label": "DateTime",
-      "placeholder": "Date Time",
-      "className": "form-control"
+      type: 'datetime-local',
+      icon: 'fa-calendar',
+      label: 'DateTime',
+      placeholder: 'Date Time',
+      className: 'form-control',
     },
     {
-      "type": "textarea",
-      "icon": "fa-text-width",
-      "label": "Textarea"
+      type: 'textarea',
+      icon: 'fa-text-width',
+      label: 'Textarea',
     },
     {
-      "type": "paragraph",
-      "icon": "fa-paragraph",
-      "label": "Paragraph",
-      "placeholder": "Type your text to display here only"
+      type: 'paragraph',
+      icon: 'fa-paragraph',
+      label: 'Paragraph',
+      placeholder: 'Type your text to display here only',
     },
     {
-      "type": "checkbox",
-      "required": true,
-      "label": "Checkbox",
-      "icon": "fa-list",
-      "description": "Checkbox",
-      "inline": true,
-      "values": [
+      type: 'checkbox',
+      required: true,
+      label: 'Checkbox',
+      icon: 'fa-list',
+      description: 'Checkbox',
+      inline: true,
+      values: [
         {
-          "label": "Option 1",
-          "value": "option-1"
+          label: 'Option 1',
+          value: 'option-1',
         },
         {
-          "label": "Option 2",
-          "value": "option-2"
-        }
-      ]
+          label: 'Option 2',
+          value: 'option-2',
+        },
+      ],
     },
     {
-      "type": "radio",
-      "icon": "fa-list-ul",
-      "label": "Radio",
-      "description": "Radio boxes",
-      "values": [
+      type: 'radio',
+      icon: 'fa-list-ul',
+      label: 'Radio',
+      description: 'Radio boxes',
+      values: [
         {
-          "label": "Option 1",
-          "value": "option-1"
+          label: 'Option 1',
+          value: 'option-1',
         },
         {
-          "label": "Option 2",
-          "value": "option-2"
-        }
-      ]
+          label: 'Option 2',
+          value: 'option-2',
+        },
+      ],
     },
     {
-      "type": "autocomplete",
-      "icon": "fa-bars",
-      "label": "Select",
-      "description": "Select",
-      "placeholder": "Select",
-      "className": "form-control",
-      "values": [
+      type: 'autocomplete',
+      icon: 'fa-bars',
+      label: 'Select',
+      description: 'Select',
+      placeholder: 'Select',
+      className: 'form-control',
+      values: [
         {
-          "label": "Option 1",
-          "value": "option-1"
+          label: 'Option 1',
+          value: 'option-1',
         },
         {
-          "label": "Option 2",
-          "value": "option-2"
+          label: 'Option 2',
+          value: 'option-2',
         },
         {
-          "label": "Option 3",
-          "value": "option-3"
-        }
-      ]
+          label: 'Option 3',
+          value: 'option-3',
+        },
+      ],
     },
     {
-      "type": "file",
-      "icon": "fa-file",
-      "label": "File Upload",
-      "className": "form-control",
-      "subtype": "file"
+      type: 'file',
+      icon: 'fa-file',
+      label: 'File Upload',
+      className: 'form-control',
+      subtype: 'file',
     },
     {
-      "type": "button",
-      "icon": "fa-paper-plane",
-      "subtype": "submit",
-      "label": "Submit"
-    }
+      type: 'button',
+      icon: 'fa-paper-plane',
+      subtype: 'submit',
+      label: 'Submit',
+    },
   ];
 
   modelFields: Array<field> = [];
@@ -166,143 +165,104 @@ export class EditAppComponent implements OnInit {
     name: 'App name...',
     description: 'App Description...',
     theme: {
-      bgColor: "ffffff",
-      textColor: "555555",
-      bannerImage: ""
+      bgColor: 'ffffff',
+      textColor: '555555',
+      bannerImage: '',
     },
-    attributes: this.modelFields
+    attributes: this.modelFields,
   };
 
   report = false;
   reports: any = [];
 
-  constructor(
-    private route: ActivatedRoute
-  ) { }
+  constructor(private route: ActivatedRoute) {}
 
-  ngOnInit() {
-    // this.route.params.subscribe( params =>{
-    //   console.log(params);
-    //   this.us.getDataApi('/admin/getFormById',{id:params.id}).subscribe(r=>{
-    //     console.log(r);
-    //     this.model = r['data'];
-    //   });
-    // });
-    // this.model = this.cs.data; 
-    // console.log(this.model.data);
-  }
+  ngOnInit() {}
 
   onDragStart(event: DragEvent) {
-    console.log("drag started", JSON.stringify(event, null, 2));
+    console.log('drag started', JSON.stringify(event, null, 2));
   }
 
   onDragEnd(event: DragEvent) {
-    console.log("drag ended", JSON.stringify(event, null, 2));
+    console.log('drag ended', JSON.stringify(event, null, 2));
   }
 
   onDraggableCopied(event: DragEvent) {
-    console.log("draggable copied", JSON.stringify(event, null, 2));
+    console.log('draggable copied', JSON.stringify(event, null, 2));
   }
 
   onDraggableLinked(event: DragEvent) {
-    console.log("draggable linked", JSON.stringify(event, null, 2));
+    console.log('draggable linked', JSON.stringify(event, null, 2));
   }
 
   onDragged(item: any, list: any[], effect: DropEffect) {
-    if (effect === "move") {
+    if (effect === 'move') {
       const index = list.indexOf(item);
       list.splice(index, 1);
     }
   }
 
   onDragCanceled(event: DragEvent) {
-    console.log("drag cancelled", JSON.stringify(event, null, 2));
+    console.log('drag cancelled', JSON.stringify(event, null, 2));
   }
 
   onDragover(event: DragEvent) {
-    console.log("dragover", JSON.stringify(event, null, 2));
+    console.log('dragover', JSON.stringify(event, null, 2));
   }
 
   onDrop(event: DndDropEvent, list?: any[]) {
-    if (list && (event.dropEffect === "copy" || event.dropEffect === "move")) {
-
-      if (event.dropEffect === "copy")
+    if (list && (event.dropEffect === 'copy' || event.dropEffect === 'move')) {
+      if (event.dropEffect === 'copy')
         event.data.name = event.data.type + '-' + new Date().getTime();
       let index = event.index;
-      if (typeof index === "undefined") {
+      if (typeof index === 'undefined') {
         index = list.length;
       }
       list.splice(index, 0, event.data);
     }
   }
 
+  /* Add options pour les champs radio , checkbox et select */
   addValue(values) {
     values.push(this.value);
-    this.value = { label: "", value: "" };
+    this.value = { label: '', value: '' };
   }
 
+  /* Méthode qui pemet supprimer un champs de la list de champ du formulaire */
   removeField(i) {
-    swal.fire({
-      title: 'Are you sure?',
-      text: "Do you want to remove this field?",
-      icon: 'warning',
-      showCancelButton: true,
-      confirmButtonColor: '#00B96F',
-      cancelButtonColor: '#d33',
-      confirmButtonText: 'Yes, remove!'
-    }).then((result) => {
-      if (result.value) {
-        this.model.attributes.splice(i, 1);
-      }
-    });
-
-  }
-
-  updateForm() {
-    let input = new FormData;
-    input.append('id', this.model._id);
-    input.append('name', this.model.name);
-    input.append('description', this.model.description);
-    input.append('bannerImage', this.model.theme.bannerImage);
-    input.append('bgColor', this.model.theme.bgColor);
-    input.append('textColor', this.model.theme.textColor);
-    input.append('attributes', JSON.stringify(this.model.attributes));
-
-    // this.us.putDataApi('/admin/updateForm',input).subscribe(r=>{
-    //   console.log(r);
-    //   swal('Success','App updated successfully','success');
-    // });
-  }
-
-  initReport() {
-    this.report = true;
-    let input = {
-      id: this.model._id
-    }
-    // this.us.getDataApi('/admin/allFilledForms',input).subscribe(r=>{
-    //   this.reports = r.data;
-    //   console.log('reports',this.reports);
-    //   this.reports.map(records=>{
-    //     return records.attributes.map(record=>{
-    //       if(record.type=='checkbox'){
-    //         record.value = record.values.filter(r=>r.selected).map(i=>i.value).join(',');
-    //       }
-    //     })
-    //   });
-    // });
+    swal
+      .fire({
+        title: 'Êtes-vous sûr?',
+        text: 'Voulez-vous supprimer ce champ?',
+        icon: 'warning',
+        showCancelButton: true,
+        confirmButtonColor: '#00B96F',
+        cancelButtonColor: '#d33',
+        confirmButtonText: 'Oui, supprimer!',
+      })
+      .then((result) => {
+        if (result.value) {
+          this.model.attributes.splice(i, 1);
+        }
+      });
   }
 
   toggleValue(item) {
     item.selected = !item.selected;
   }
 
+  /* Bouton Submit, à des fins de validation dans l'aperçu du formulaire */
   submit() {
     let valid = true;
     let validationArray = JSON.parse(JSON.stringify(this.model.attributes));
-    validationArray.reverse().forEach(field => {
-      console.log(field.label + '=>' + field.required + "=>" + field.value);
+    validationArray.reverse().forEach((field) => {
+      console.log(field.label + '=>' + field.required + '=>' + field.value);
       if (field.required && !field.value && field.type != 'checkbox') {
-        swal.fire('Error', 'Please enter ' + field.label, 'error');
+        swal.fire(
+          'Error',
+          'Veuillez saisir information dans le champ ' + field.label,
+          'error'
+        );
         valid = false;
         return false;
       }
@@ -315,28 +275,23 @@ export class EditAppComponent implements OnInit {
         }
       }
       if (field.required && field.type == 'checkbox') {
-        if (field.values.filter(r => r.selected).length == 0) {
-          swal.fire('Error', 'Please enterrr ' + field.label, 'error');
+        if (field.values.filter((r) => r.selected).length == 0) {
+          swal.fire(
+            'Error',
+            'Veuillez saisir information dans le champ ' + field.label,
+            'error'
+          );
           valid = false;
           return false;
         }
-
       }
     });
     if (!valid) {
       return false;
     }
     console.log('Save', this.model);
-    let input = new FormData;
+    let input = new FormData();
     input.append('formId', this.model._id);
-    input.append('attributes', JSON.stringify(this.model.attributes))
-    // this.us.postDataApi('/user/formFill',input).subscribe(r=>{
-    //   console.log(r);
-    //   swal('Success','You have contact sucessfully','success');
-    //   this.success = true;
-    // },error=>{
-    //   swal('Error',error.message,'error');
-    // });
+    input.append('attributes', JSON.stringify(this.model.attributes));
   }
-
 }
