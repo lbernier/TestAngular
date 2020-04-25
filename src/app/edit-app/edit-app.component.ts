3d33afbc669295ea74315f353d9a51e385abd348
lbernier <leandro_bernier@yahoo>
2020-04-25 19:43:33 -0400

    mejoras en el codigo  y creacion de un archivo de controles tratados

--- src/app/edit-app/edit-app.component.ts
+++ src/app/edit-app/edit-app.component.ts
@@ -3,6 +3,7 @@ import { DndDropEvent, DropEffect } from 'ngx-drag-drop';
 import { field, value } from '../global.model';
 import { ActivatedRoute } from '@angular/router';
 import swal from 'sweetalert2';
+import { contrôlesPrisEnCharge } from './contrôles-pris-en-charge';
 
 @Component({
   selector: 'app-edit-app',
@@ -14,151 +15,8 @@ export class EditAppComponent implements OnInit {
     label: '',
     value: '',
   };
-  success = false;
 
-  fieldModels: Array<field> = [
-    {
-      type: 'text',
-      icon: 'fa-font',
-      label: 'Text',
-      description: 'Enter your name',
-      placeholder: 'Enter your name',
-      className: 'form-control',
-      subtype: 'text',
-      regex: '',
-      handle: true,
-    },
-    {
-      type: 'email',
-      icon: 'fa-envelope',
-      required: true,
-      label: 'Email',
-      description: 'Enter your email',
-      placeholder: 'Enter your email',
-      className: 'form-control',
-      subtype: 'text',
-      regex: '^([a-zA-Z0-9_.-]+)@([a-zA-Z0-9_.-]+).([a-zA-Z]{2,5})$',
-      errorText: 'Please enter a valid email',
-      handle: true,
-    },
-    {
-      type: 'phone',
-      icon: 'fa-phone',
-      label: 'Phone',
-      description: 'Enter your phone',
-      placeholder: 'Enter your phone',
-      className: 'form-control',
-      subtype: 'text',
-      regex: '^[+]?[(]?[0-9]{3}[)]?[-s.]?[0-9]{3}[-s.]?[0-9]{4,6}$',
-      errorText: 'Please enter a valid phone number',
-      handle: true,
-    },
-    {
-      type: 'number',
-      label: 'Number',
-      icon: 'fa-html5',
-      description: 'Age',
-      placeholder: 'Enter your age',
-      className: 'form-control',
-      value: '20',
-      min: 12,
-      max: 90,
-    },
-    {
-      type: 'date',
-      icon: 'fa-calendar',
-      label: 'Date',
-      placeholder: 'Date',
-      className: 'form-control',
-    },
-    {
-      type: 'datetime-local',
-      icon: 'fa-calendar',
-      label: 'DateTime',
-      placeholder: 'Date Time',
-      className: 'form-control',
-    },
-    {
-      type: 'textarea',
-      icon: 'fa-text-width',
-      label: 'Textarea',
-    },
-    {
-      type: 'paragraph',
-      icon: 'fa-paragraph',
-      label: 'Paragraph',
-      placeholder: 'Type your text to display here only',
-    },
-    {
-      type: 'checkbox',
-      required: true,
-      label: 'Checkbox',
-      icon: 'fa-list',
-      description: 'Checkbox',
-      inline: true,
-      values: [
-        {
-          label: 'Option 1',
-          value: 'option-1',
-        },
-        {
-          label: 'Option 2',
-          value: 'option-2',
-        },
-      ],
-    },
-    {
-      type: 'radio',
-      icon: 'fa-list-ul',
-      label: 'Radio',
-      description: 'Radio boxes',
-      values: [
-        {
-          label: 'Option 1',
-          value: 'option-1',
-        },
-        {
-          label: 'Option 2',
-          value: 'option-2',
-        },
-      ],
-    },
-    {
-      type: 'autocomplete',
-      icon: 'fa-bars',
-      label: 'Select',
-      description: 'Select',
-      placeholder: 'Select',
-      className: 'form-control',
-      values: [
-        {
-          label: 'Option 1',
-          value: 'option-1',
-        },
-        {
-          label: 'Option 2',
-          value: 'option-2',
-        },
-        {
-          label: 'Option 3',
-          value: 'option-3',
-        },
-      ],
-    },
-    {
-      type: 'file',
-      icon: 'fa-file',
-      label: 'File Upload',
-      className: 'form-control',
-      subtype: 'file',
-    },
-    {
-      type: 'button',
-      icon: 'fa-paper-plane',
-      subtype: 'submit',
-      label: 'Submit',
-    },
-  ];
+  fieldModels: Array<field> = contrôlesPrisEnCharge;
 
   modelFields: Array<field> = [];
   model: any = {
@@ -288,6 +146,12 @@ export class EditAppComponent implements OnInit {
     });
     if (!valid) {
       return false;
+    } else {
+      swal.fire(
+        'Le formulaire a été soumis avec succès.',
+        'cliquez sur le bouton pour continuer.',
+        'success'
+      );
     }
     console.log('Save', this.model);
     let input = new FormData();
